268d69dc0e36047bad9a4ffbf9e4576e83947274
Edilson Alexandre Cuamba <edilson.cuamba@exi.co.mz>
2022-06-15 10:58:59 +0200

    Adicionei a funcionalidade para pesquisar publicações na p]agina inicial.

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,10 +9,17 @@ class Listing extends Model
 {
     use HasFactory;
 
-    public function scopeFilter($query, array $filters){
+    public function scopeFilter($query, array $filters)
+    {
         //Esta instrição verifica se temos uma tag na variavel $filters, se tivermos ela é true e se não ela é false e vai para o else.
-        if($filters['tag'] ?? false){
-            $query->where('tags', 'like','%' . request('tag') . '%');
+        if ($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('description', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
     }
 }
